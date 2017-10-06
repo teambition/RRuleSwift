3453320c84b4e254b6eeb97313d1cc846f3f1d45
Tom Quist <tom@quist.de>
2017-10-06 11:36:28 +0200

    Added cocoapods support

--- Sources/JavaScriptBridge.swift
+++ Sources/JavaScriptBridge.swift
@@ -10,8 +10,12 @@ import Foundation
 import EventKit
 
 internal struct JavaScriptBridge {
+    
+    // This class is needed to be able to get out bundle
+    private class DummyClass {}
+    
     internal static func rrulejs() -> String? {
-        let libPath = Bundle(identifier: "Teambition.RRuleSwift-iOS")?.path(forResource: "rrule", ofType: "js") ?? Bundle.main.path(forResource: "rrule", ofType: "js")
+        let libPath = Bundle(for: DummyClass.self).path(forResource: "rrule", ofType: "js") ?? Bundle.main.path(forResource: "rrule", ofType: "js")
         guard let rrulelibPath = libPath else {
             return nil
         }
