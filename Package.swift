cc578736a5fa5c47db48e6478d4fc0cd6c0e11ed
Martynas Limontas <martynas@jumsoft.com>
2021-10-08 11:43:10 +0300

    Package.swift (in progress)

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "RRuleSwift-iOS",
             dependencies: [],
-            path: ""
+            path: "Sources"
         ),
     ]
 )
