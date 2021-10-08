0dff8ef52c4662004e7c4e0d467ad33396b3c200
Martynas Limontas <martynas@jumsoft.com>
2021-10-08 13:03:16 +0300

    Package.swift (in progress)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RRuleSwift-iOS",
+            name: "RRuleSwift",
             targets: ["RRuleSwift-iOS"]),
     ],
     dependencies: [
