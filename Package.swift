e10188bccabc1da92f3cf6274f717e5b3fad95d2
Martynas Limontas <martynas@jumsoft.com>
2021-10-08 11:27:57 +0300

    Package.swift (in progress)

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RRuleSwift-iOS",
-            dependencies: [] //,
-//            path: "RRuleSwift-iOS"
+            dependencies: [],
+            path: ""
         ),
     ]
 )
