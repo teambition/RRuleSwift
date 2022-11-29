58f2f65578b1c0fefbb1294b272718e06501acef
Nikhil Nigade <nikhil@dezinezync.com>
2022-11-29 06:32:53 +0530

    generate XCFramework target

---
+++ RRule/RRule.h
@@ -0,0 +1,19 @@
+//
+//  RRule.h
+//  RRule
+//
+//  Created by Nikhil Nigade on 28/11/22.
+//  Copyright © 2022 Teambition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RRule.
+FOUNDATION_EXPORT double RRuleVersionNumber;
+
+//! Project version string for RRule.
+FOUNDATION_EXPORT const unsigned char RRuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RRule/PublicHeader.h>
+
+
